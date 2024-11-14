5744c4a012054f251eed115b503aa89ac705663a
Your Actual Name <your.email@example.com>
2024-11-13 22:46:07 -0500

    assignment 6

---
+++ src/app/pipes/full-name.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Phone } from "../models/phone";
+@Pipe({
+  name: 'fullName',
+  standalone: true
+})
+export class FullNamePipe implements PipeTransform {
+  transform(phone: Phone): string {
+    return `${phone.firstName} ${phone.lastName}`;
+  }
+}
---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,22 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Phone } from '../models/phone';
+
+export class InMemoryDataService implements InMemoryDbService {
+  //returns an object with a students property,
+  // which is an array of User objects
+  createDb():{contents: Phone[]} {
+    /*
+    Inside the method, a constant array named students is defined,
+    containing several User objects. Each User object represents a
+    student with properties such as id, firstName, lastName,
+     department, and isAdmin. For example, one of the User objects is
+     */
+    const contents: Phone[] = [
+      { id: 1, brand: 'Apple', name: 'iPhone 14', colour: 'Black',  typec: true },
+      { id: 2, brand: 'Samsung', name: 'Galaxy S21', colour: 'Silver',  typec: true },
+      { id: 3, brand: 'Google', name: 'Pixel 7', colour: 'White',  typec: true },
+      { id: 4, brand: 'OnePlus', name: '9 Pro', colour: 'Green',  typec: true }
+    ];
+    return { contents };
+  }
+}
